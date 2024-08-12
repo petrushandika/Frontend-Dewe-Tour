b509d5d1795d3798a7d5b3606397f9ec24371f53
petrushandika <petrushandikasinaga@gmail.com>
2024-08-13 00:21:35 +0700

    chore: setup structure folder & file

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-between items-center">
+    <img
+      src="../../public/logo.png"
+      alt=""
+      className="w-48"
+    />
+    <div className="flex flex-row gap-5">
+      <button
+        className="w-28 h-8 border border-white rounded-md text-white text-base bg-transparent"
+      >
+        Login
+      </button>
+      <button
+        className="w-28 h-8 border border-orange-400 rounded-md text-white text-base bg-orange-400"
+      >
+        Register
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ src/pages/Home/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div className="bg-black">
+    <Header />
+  </div>
+</template>
+
+<script setup>
+import Header from "../components/Header.vue";
+</script>
